c2a06e2f36791546b55703e6bebc9b25322485b3
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-14 05:30:15 +0200

    - Exploring Routes with Spring Cloud Gateway

---
+++ src/main/java/com/mytech/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,34 @@
+package com.mytech.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange-service"))
+                .route(p -> p.path("/currency-converter/**")
+                        .uri("lb://currency-conversion-service"))
+                .route(p -> p.path("/currency-converter-feign/**")
+                        .uri("lb://currency-conversion-service"))
+                .route(p -> p.path("/currency-converter-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-converter-new/(?<segment>.*)",
+                                "/currency-converter-feign/${segment}"))
+                        .uri("lb://currency-conversion-service"))
+                .build();
+    }
+
+}
